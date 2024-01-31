d3ad7e26d5ae5610f338b3264ac4e23c8c1706eb
swwayamm <codingswayam24@gmail.com>
2024-01-31 23:56:33 +0530

    Basic Setup Ready ( tailwind working )

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="w-[10rem] h-[7rem] border-gray-600 border-2">
+      <div className="flex justify-center   ">
+        <h6 >1</h6>
+      </div>
+      
+    </div>
+  )
+}

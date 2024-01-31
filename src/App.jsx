5150b07cf367cf4226cb44b50e37553672946ca2
swwayamm <codingswayam24@gmail.com>
2024-01-31 23:58:43 +0530

    Task for next day

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 export default function App() {
   return (
+
+    // TODO: Have A Proper UI
+    // TODO: Implement the Event in the box
     <div className="w-[10rem] h-[7rem] border-gray-600 border-2">
       <div className="flex justify-center   ">
         <h6 >1</h6>
